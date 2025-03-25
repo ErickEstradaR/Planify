34f22480f89c9fbfdb6284b9b47a9285ccdc8aab
erick <erickestradarosario@gmail.com>
2025-03-25 13:01:53 -0400

    Crea modelo Presupuestos

---
+++ Planify/Models/Presupuestos.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Planify.Models;
+
+public class Presupuestos
+{
+    [Key]
+    public int PresupuestoId { get; set; }
+    
+    public DateTime Fecha { get; set; }
+    
+    [ForeignKey("EventoId")]
+    public Eventos Evento { get; set; }
+    
+    public int EventoId { get; set; }
+    
+    public List<PresupuestosDetalle> PresupuestosDetalles { get; set; } = new List<PresupuestosDetalle>();
+}
