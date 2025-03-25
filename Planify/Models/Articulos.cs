bb06399a0ea28bd03cb580ea68ce0486f1d3fdda
erick <erickestradarosario@gmail.com>
2025-03-25 13:01:39 -0400

    Crea Modelo Articulos

---
+++ Planify/Models/Articulos.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Planify.Models;
+
+public class Articulos
+{
+    [Key]
+    public int ArticuloId { get; set; }
+    
+    public double Precio { get; set; }
+    
+    public string Nombre { get; set; }
+    
+    public string Descripcion { get; set; }
+    
+    public string Categoria { get; set; }
+}
