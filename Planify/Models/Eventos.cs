d2147f481ea13ba77557fd34a128c4ed3ee1f652
erick <erickestradarosario@gmail.com>
2025-03-25 13:01:46 -0400

    Crea modelo Eventos

---
+++ Planify/Models/Eventos.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Planify.Models;
+
+public class Eventos
+{
+    
+    [Key]
+    public int EventoId { get; set; }
+    
+    public string Categoria { get; set; }
+    
+    public string Descripcion { get; set; }
+    
+    public string Direccion { get; set; }
+    
+    public DateTime FechaEvento { get; set; }
+    
+    
+    public int ClienteId { get; set; }
+    
+    [ForeignKey("ClienteId")]
+    public Clientes? Cliente { get; set; }
+}
