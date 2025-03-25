61fad5bb05e8f7c063a05db07e8de04530cb68e0
erick <erickestradarosario@gmail.com>
2025-03-25 13:01:59 -0400

    Crea modelo PresupuestosDetalle

---
+++ Planify/Models/PresupuestosDetalle.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Planify.Models;
+
+public class PresupuestosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+    
+    
+    public int PresupuestoId { get; set; }
+    [ForeignKey("PresupuestoId")]
+    public Presupuestos Presupuesto { get; set; }
+    
+    public int ArticuloId { get; set; }
+    [ForeignKey("ArticuloId")]
+    public Articulos Articulo { get; set; }
+}
