d80529948e3661541f65c5c3550b1fbfb17bc013
erick <erickestradarosario@gmail.com>
2025-03-25 20:29:24 -0400

    Edité el contexto del Service

--- Planify/Services/EventosService.cs
+++ Planify/Services/EventosService.cs
@@ -1,11 +1,11 @@
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
-using Planify.DAL;
+using Planify.Data;
 using Planify.Models;
 
 namespace Planify.Services;
 
-public class EventosService (IDbContextFactory<Contexto> dbfactory)
+public class EventosService (IDbContextFactory<ApplicationDbContext> dbfactory)
 {
     /// <summary>
     /// Este método inserta o modifica un evento en la base de datos dependiendo de si este existe o no
