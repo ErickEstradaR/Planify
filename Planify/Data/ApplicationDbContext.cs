d9ba54aca42f36dfb7e861958bcf899bd3ad9de2
erick <erickestradarosario@gmail.com>
2025-03-25 20:25:57 -0400

    añadi dbSets de los modelos

--- Planify/Data/ApplicationDbContext.cs
+++ Planify/Data/ApplicationDbContext.cs
@@ -1,9 +1,15 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Planify.Models;
 
 namespace Planify.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-    : IdentityDbContext<ApplicationUser>(options)
+    : IdentityDbContext<ApplicationUser>(options) 
 {
+    public DbSet<Articulos> Articulos { get; set; }
+    public DbSet<Eventos> Eventos { get; set; }
+    public DbSet<Clientes> Clientes { get; set; }
+    public DbSet<Presupuestos> Presupuestos { get; set; }
+    public DbSet<PresupuestosDetalle> PresupuestosDetalles { get; set; }
 }
