5f5d4ef31df81d3c4860134f2302469244d06e44
erick <erickestradarosario@gmail.com>
2025-03-28 13:36:09 -0400

    Añade validaciones a Eventos

--- Planify/Models/Eventos.cs
+++ Planify/Models/Eventos.cs
@@ -9,15 +9,22 @@ public class Eventos
     [Key]
     public int EventoId { get; set; }
     
+    [Required(ErrorMessage = "El nombre del evento es obligatorio.")]
     public string NombreEvento { get; set; }
+
+    [Required(ErrorMessage = "La categoría es obligatoria.")]
     public string Categoria { get; set; }
-    
+
+    [Required(ErrorMessage = "La descripción es obligatoria.")]
     public string Descripcion { get; set; }
-    
+
+    [Required(ErrorMessage = "La dirección es obligatoria.")]
     public string Direccion { get; set; }
-    
+
+    [Required(ErrorMessage = "La fecha del evento es obligatoria.")]
     public DateTime FechaEvento { get; set; }
-    
+
+    [Required(ErrorMessage = "El UserId es obligatorio.")]
     public string UserId { get; set; }
     
     public int ClienteId { get; set; }
