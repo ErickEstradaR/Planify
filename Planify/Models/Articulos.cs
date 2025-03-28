e09ce02eaa49b442a88156f9dc78747f15905fc4
erick <erickestradarosario@gmail.com>
2025-03-28 13:38:11 -0400

    Validaciones de Articulos

--- Planify/Models/Articulos.cs
+++ Planify/Models/Articulos.cs
@@ -7,10 +7,15 @@ public class Articulos
     [Key]
     public int ArticuloId { get; set; }
     
+    [Required,Range(1, int.MaxValue)]
     public double Precio { get; set; }
-    
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [MinLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres.")]
+    [MaxLength(50, ErrorMessage = "El nombre no puede superar los 50 caracteres.")]
     public string Nombre { get; set; }
-    
+    [Required(ErrorMessage = "La descripción es obligatoria.")]
+    [MinLength(6, ErrorMessage = "Debe tener al menos 6 caracteres.")]
+    [MaxLength(100, ErrorMessage = "No puede superar los 100 caracteres.")]
     public string Descripcion { get; set; }
     
     public string Categoria { get; set; }
