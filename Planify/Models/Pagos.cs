72b7ab0978776eb49298feb457ba1d0f3bd14d9f
erick <erickestradarosario@gmail.com>
2025-04-02 13:08:43 -0400

    Crea modelo Pagos

---
+++ Planify/Models/Pagos.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Planify.Models;
+
+public class Pagos
+{
+    [Key]
+    public int PagoId { get; set; } 
+    public double Monto { get; set; } 
+    public DateTime FechaCobro { get; set; } 
+    public string HoraEnvio { get; set; }
+    public string DireccionEnvio { get; set; }
+    
+    public int PresupuestoId { get; set; }
+    [ForeignKey("PresupuestoId")]
+    public Presupuestos Presupuesto { get; set; }
+
+    
+    public int TarjetaId { get; set; }
+    [ForeignKey("TarjetaId")]
+    public TarjetasCredito Tarjeta { get; set; }
+}
