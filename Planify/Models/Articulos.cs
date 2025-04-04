9a99cdbd354076bd32507ca41c4efc95d2d2038a
erick <erickestradarosario@gmail.com>
2025-04-04 13:24:55 -0400

    Modifica validaciones

--- Planify/Models/Articulos.cs
+++ Planify/Models/Articulos.cs
@@ -7,17 +7,19 @@ public class Articulos
     [Key]
     public int ArticuloId { get; set; }
     
-    [Required,Range(1, int.MaxValue)]
+    [Required(ErrorMessage = "Debe ingresar un valor en Precio.")]
+     [Range(1, int.MaxValue,ErrorMessage = "El precio debe ser mayor a 1") ] 
     public double Precio { get; set; }
     [Required(ErrorMessage = "El nombre es obligatorio.")]
     [MinLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres.")]
     [MaxLength(50, ErrorMessage = "El nombre no puede superar los 50 caracteres.")]
     public string Nombre { get; set; }
     [Required(ErrorMessage = "La descripción es obligatoria.")]
-    [MinLength(6, ErrorMessage = "Debe tener al menos 6 caracteres.")]
-    [MaxLength(100, ErrorMessage = "No puede superar los 100 caracteres.")]
+    [MinLength(4, ErrorMessage = " La descripción debe tener al menos 6 caracteres.")]
+    [MaxLength(100, ErrorMessage = "La descripción no puede superar los 100 caracteres.")]
     public string Descripcion { get; set; }
     
+    [Required(ErrorMessage = "Debe seleccionar una categoría")]
     public string Categoria { get; set; }
     
 }
